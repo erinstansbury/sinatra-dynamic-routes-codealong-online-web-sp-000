ceeb161240131c1e0edba43a5918a37b06528664
Erin Stansbury <ekstansbury@gmail.com>
2020-10-04 21:31:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,9 +18,10 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}!"
   end
   
+  get '/multiply/:num1/:num2' do
     @num1 = params[:num1].to_i
     @num2 = params[:num2].to_i 
     @total = (@num1 * @num2)
-    # erb :multiply
     "The product is: #{@total}."
+  end
 end
